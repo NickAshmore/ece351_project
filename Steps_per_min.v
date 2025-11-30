e067b927b7bed36852421a2b6cc7f4bbc56a0f29
hreaves <hreaves1@vols.utk.edu>
2025-11-29 21:53:26 -0600

    comments

--- Steps_per_min.v
+++ Steps_per_min.v
@@ -8,16 +8,16 @@ module steps_per_min(input clk, rst, [15:0]step_count, output [15:0]spm);
     
     always@(posedge clk) begin
         if(rst) begin
-            sec_count <= 0;
-            cycle_count <= 0;
+            sec_count <= 0; // seconds counter
+            cycle_count <= 0; // cycle counter
         end
         else begin
-            if(cycle_count == 27'd99_999_999) begin
+            if(cycle_count == 27'd99_999_999) begin // the second counter increments every 100_000_000 10 ns cycles
                 cycle_count <= 0;
                 sec_count <= sec_count + 1;
             end
             else
-                cycle_count <= cycle_count + 1; 
+                cycle_count <= cycle_count + 1; // increase cycle counter every clock edge 
         end
     end   
  
@@ -25,7 +25,7 @@ module steps_per_min(input clk, rst, [15:0]step_count, output [15:0]spm);
         if(rst)
             r_spm <= 0;
         else if (sec_count != 0)
-            r_spm <= (step_count * 16'd60) / sec_count;
+            r_spm <= (step_count * 16'd60) / sec_count; // total steps * 60 seconds / total seconds equals steps per minute
     end
     
     assign spm = r_spm;
